2526fc6a2ae417efac3d80e05e2d832816be4e48
zm <68761971+szm8991@users.noreply.github.com>
2022-06-05 10:17:56 +0800

    upadte: day2

--- day2/script.js
+++ day2/script.js
@@ -25,8 +25,7 @@ function update() {
       circle.classList.remove('active');
     }
   });
-  const actives = document.querySelectorAll('.active');
-  progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+  progress.style.width = ((currentActive - 1) / (circles.length - 1)) * 100 + '%';
   if (currentActive === 1) {
     prev.disabled = true;
   } else if (currentActive === circles.length) {
