81aba27a483285c3390940da4932ac6f07f275dc
szm <songzheming157@163.com>
2022-06-22 20:11:49 +0800

    update: 27-30

---
+++ day27/script.js
@@ -0,0 +1,44 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = ['Message One', 'Message Two', 'Message Three', 'Message Four']
+
+const types = ['info', 'success', 'error']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div')
+
+  notif.addEventListener('transitionend', () => notif.remove())
+
+  notif.classList.add('toast')
+
+  notif.classList.add(type ? type : getRandom('type'))
+
+  notif.innerText = message ? message : getRandom('message')
+
+  toasts.appendChild(notif)
+
+  setTimeout(() => {
+    notif.style.opacity = 0
+  }, 2000)
+}
+
+function getRandom(type) {
+  switch (type) {
+    case 'message':
+      return getRandomMessage()
+    case 'type':
+      return getRandomType()
+    case 'default':
+      return
+  }
+}
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)]
+}
